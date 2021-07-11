9a462cd5dbe853b0abe29c5ffafabb626ed02c22
cerebus <adrogunova@gmail.com>
2021-07-11 19:38:31 +0300

    -  сделала printError TODO: выход из программы при ошибке - в стандартный ввод пишет, кгда нет файла

--- getBinaryName.c
+++ getBinaryName.c
@@ -12,6 +12,11 @@ void getBinaryName(char **command)
     i = 0;
     while(s[i])
     {
+        if(!ft_strncmp(s, "/bin/", 5))
+        {
+            *command = ft_substr(&(s[5]), 0, ft_strlen(&(s[5])));
+            return;
+        }
         if (s[i] == ' ')
         {
             *command = ft_substr(s, 0, i);
