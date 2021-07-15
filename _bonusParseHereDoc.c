d23664a75ec23c13df88d19f44871ffb84b695f6
cerebus <adrogunova@gmail.com>
2021-07-15 18:18:39 +0300

    RU: 1. близка к разгадке задачи

--- _bonusParseHereDoc.c
+++ _bonusParseHereDoc.c
@@ -12,29 +12,53 @@ int _bonusParseHereDoc(char **argv, char **pathList, int argc)
     char **execArr;
     int fileFd;
     char *tmp;
+    char *str;
 
     line = NULL;
+    str = malloc(1 * sizeof (char));
+    str[0] = '\0';
     fd = malloc(2 * sizeof (int));
     pipe(fd);
 
-    while (!line || ft_strcmp(line, argv[2]))
-    {
-        write(1, "> ", 2);
-        tmp = line;
-        line = NULL;
-        free(tmp);
-        get_next_line(STDIN_FILENO, &line);
-    }
-    printf("END!\n");
+//    while (!line || ft_strcmp(line, argv[2]))
+//    {
+//        write(1, "> ", 2);
+//       // line = NULL;
+//        get_next_line(STDIN_FILENO, &line);
+//        tmp = str;
+//        str = ft_strjoin(str, line);
+//        free(tmp);
+//        printf("line = %s\n", line);
+//        printf("str = %s\n", str);
+//    }
+//    printf("END!\n");
+
+   // write(0, str, ft_strlen(str));
+    //int res = write(0, str, ft_strlen(str));
+    //printf("res = %d\n", res);
     pid = fork();
     if (pid < 0)
         return (2);
     if(pid == 0)
     {
+        while (!line || ft_strcmp(line, argv[2]))
+        {
+            write(1, "> ", 2);
+            // line = NULL;
+            get_next_line(STDIN_FILENO, &line);
+            tmp = str;
+            str = ft_strjoin(str, line);
+            free(tmp);
+            printf("line = %s\n", line);
+            printf("str = %s\n", str);
+        }
         command = argv[3];
+        write(0, str, ft_strlen(str));
+//        int res =  write(0, "\0", 1);
+//        printf("res = %d\n", res);
         execArr = getExecArr(command, pathList);
-        close(fd[0]);
         dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
         close(fd[1]);
         execve(execArr[0], execArr, NULL);
     }
@@ -43,24 +67,21 @@ int _bonusParseHereDoc(char **argv, char **pathList, int argc)
         return (2);
     if(pid == 0) {
         command = argv[argc - 2];
-        printf("command = %s\n", command);
-        dup2(fd[0], STDIN_FILENO);
         execArr = getExecArr(command, pathList);
         if(!access(argv[argc - 1], 0))
             fileFd = open(argv[argc - 1], O_RDWR | O_APPEND); //открываем файл, из которого берём данные
         else
             fileFd = open(argv[argc - 1], O_CREAT | O_RDWR, 0644);
         dup2(fileFd, STDOUT_FILENO);
+        dup2(fd[0], STDIN_FILENO);
         close(fd[1]);
         close(fd[0]);
         close(fileFd);
         write(1, "VVV\n", 4);
         execve(execArr[0], execArr, NULL);
-        write(1, "VVV\n", 4);
     }
     close(fd[0]);
     close(fd[1]);
-
     free(fd);
     waitChildren();
     return (0);
