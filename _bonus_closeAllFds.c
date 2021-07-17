9d368ac8a8a95f8d4c3523909af4a2111c5e282b
cerebus <adrogunova@gmail.com>
2021-07-17 17:34:29 +0300

    normed _bonus_closeAllFds.c

--- _bonus_closeAllFds.c
+++ _bonus_closeAllFds.c
@@ -1,23 +1,19 @@
-//
-// Created by Cesar Erebus on 7/11/21.
-//
-
 #include <unistd.h>
 #include <stdlib.h>
 
-void _bonus_closeAllFds(int ***fd, int commands_num)
+void	_bonus_closeAllFds(int ***fd, int commands_num)
 {
-    int i;
+	int	i;
+	int	**tmp;
 
-    i = 0;
-    int **tmp = *fd;
-
-    while (i < commands_num)
-    {
-        close(tmp[i][0]);
-        close(tmp[i][1]);
-        free(tmp[i]);
-        tmp[i] = NULL;
-        i++;
-    }
+	i = 0;
+	tmp = *fd;
+	while (i < commands_num)
+	{
+		close(tmp[i][0]);
+		close(tmp[i][1]);
+		free(tmp[i]);
+		tmp[i] = NULL;
+		i++;
+	}
 }
