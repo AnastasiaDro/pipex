69e4827e64813f27686e16d28c0fd5153a124297
cerebus <cerebus@student.21-school.ru>
2021-07-18 13:34:04 +0300

    -added gnl-file

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cerebus <cerebus@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/21 14:12:05 by cerebus           #+#    #+#             */
+/*   Updated: 2020/12/29 01:43:30 by cerebus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		ft_increase_tail(char **tail, char **buff, int *start, int read_bytes)
+{
+	char *tail_tmp;
+
+	tail_tmp = NULL;
+	if (*start == 0)
+		*start = ft_strlen(*tail);
+	else
+		*start = *start + read_bytes;
+	(*buff)[read_bytes] = '\0';
+	tail_tmp = *tail;
+	*tail = ft_strjoin(*tail, *buff);
+	free(tail_tmp);
+	if (!(*tail))
+	{
+		free(*buff);
+		return (-1);
+	}
+	return (1);
+}
+
+int		ft_split_n_tail(char *pn, char **tail, char **line)
+{
+	char *tmp;
+
+	tmp = NULL;
+	*pn = '\0';
+	*line = ft_strdup(*tail);
+	tmp = *tail;
+	*tail = ft_strdup(++pn);
+	free(tmp);
+	tmp = NULL;
+	if (!(*line) || !(*tail))
+		return (-1);
+	return (1);
+}
+
+int		ft_get_from_tail(char **line, char **tail)
+{
+	*line = ft_strdup(*tail);
+	free(*tail);
+	*tail = NULL;
+	if (!(*line))
+		return (-1);
+	return (0);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	static char	*tail;
+	char		*buff;
+	int			read_bytes;
+	char		*pn;
+	int			start;
+
+	read_bytes = -2;
+	start = 0;
+	if (!line || (read(fd, NULL, 0) == -1) || BUFFER_SIZE < 1 ||
+		!(buff = malloc((BUFFER_SIZE + 1) * sizeof(char))))
+		return (-1);
+	buff[BUFFER_SIZE] = '\0';
+	if (!tail)
+		tail = ft_strdup("");
+	*line = NULL;
+	while (!(pn = ft_strchr(&tail[start], '\n')) &&
+		(read_bytes = read(fd, buff, BUFFER_SIZE)))
+		if (ft_increase_tail(&tail, &buff, &start, read_bytes) == -1)
+			return (-1);
+	free(buff);
+	if (pn)
+		return (ft_split_n_tail(pn, &tail, line));
+	else
+		return (ft_get_from_tail(line, &tail));
+}
