14c1e177962ec773a1b820ff6224ab95f55025e5
cerebus <cerebus@student.21-school.ru>
2021-07-20 15:18:56 +0300

    normed bonusGetStdin.c and bonusGetTmpFile.c

--- bonusGetStdin.c
+++ bonusGetStdin.c
@@ -1,30 +1,27 @@
-//
-// Created by Cesar Erebus on 7/17/21.
-//
 #include <sys/fcntl.h>
 #include "pipex.h"
 #include "pipex_bonus.h"
 
-int bonusGetStdin(int **fd, char *command, int tmpFd, t_bstruct *bStruct)
+int	bonusGetStdin(int **fd, char *command, int tmpFd, t_bstruct *bStruct)
 {
-    int pid;
-    char **execArr;
-    pid = fork();
-    if (pid < 0)
-        return (2);
-    if(pid == 0)
-    {
-        tmpFd = open("tmpFile", O_RDONLY, 0644);
-        dup2(tmpFd, STDIN_FILENO);
-        execArr = getExecArr(command, bStruct->pathList);
-        dup2(fd[1][1], STDOUT_FILENO);
-        close(tmpFd);
-        close(fd[0][0]);
-        close(fd[1][1]);
-        bonusCloseAllFds(&fd, bStruct->commands_num);
-        mFree(bStruct->pathList);
-        execve(execArr[0], execArr, NULL);
-    }
-    return (0);
-}
+	int		pid;
+	char	**execArr;
 
+	pid = fork();
+	if (pid < 0)
+		return (2);
+	if (pid == 0)
+	{
+		tmpFd = open("tmpFile", O_RDONLY, 0644);
+		dup2(tmpFd, STDIN_FILENO);
+		execArr = getExecArr(command, bStruct->pathList);
+		dup2(fd[1][1], STDOUT_FILENO);
+		close(tmpFd);
+		close(fd[0][0]);
+		close(fd[1][1]);
+		bonusCloseAllFds(&fd, bStruct->commands_num);
+		mFree(bStruct->pathList);
+		execve(execArr[0], execArr, NULL);
+	}
+	return (0);
+}
--- bonusGetTmpFile.c
+++ bonusGetTmpFile.c
@@ -1,31 +1,27 @@
-//
-// Created by Cesar Erebus on 7/17/21.
-//
-
 #include <stdlib.h>
 #include <unistd.h>
 #include "libft/libft.h"
-# include "get_next_line/get_next_line.h"
+#include "get_next_line/get_next_line.h"
 
-int  bonusGetTmpFile(char **argv)
+int	bonusGetTmpFile(char **argv)
 {
-    char *line;
-    int tFileFd;
-    char *tmp;
+	char	*line;
+	int		tFileFd;
+	char	*tmp;
 
-    line = NULL;
-    tFileFd = open("tmpFile", O_CREAT | O_RDWR, 0644);
-    write(1, "> ", 2);
-    get_next_line(STDIN_FILENO, &line);
-    while (ft_strcmp(line, argv[2]))
-    {
-        tmp = line;
-        write(tFileFd, line, ft_strlen(line));
-        write(tFileFd, "\n", 1);
-        write(1, "> ", 2);
-        get_next_line(STDIN_FILENO, &line);
-        free(tmp);
-    }
-    free(line);
-    return (tFileFd);
+	line = NULL;
+	tFileFd = open("tmpFile", O_CREAT | O_RDWR, 0644);
+	write(1, "> ", 2);
+	get_next_line(STDIN_FILENO, &line);
+	while (ft_strcmp(line, argv[2]))
+	{
+		tmp = line;
+		write(tFileFd, line, ft_strlen(line));
+		write(tFileFd, "\n", 1);
+		write(1, "> ", 2);
+		get_next_line(STDIN_FILENO, &line);
+		free(tmp);
+	}
+	free(line);
+	return (tFileFd);
 }
