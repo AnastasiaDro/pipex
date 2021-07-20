23d77e773e4623cf65ca238b51eddd7d66f4a8dc
cerebus <cerebus@student.21-school.ru>
2021-07-20 20:54:18 +0300

    -normed all except main.c

--- parseLastCommand.c
+++ parseLastCommand.c
@@ -2,35 +2,30 @@
 #include <sys/fcntl.h>
 #include "pipex_bonus.h"
 
-int	parseLastCommand(int **fd, t_bstruct *bStruct)
+int	parseLastCommand(int **fd, t_bstruct *bS)
 {
-	int pid;
-	char *command;
-	char **execArr;
-	int fileFd;
+	int		pid;
+	char	*command;
+	char	**execArr;
+	int		fileFd;
 
 	pid = fork();
-	if (pid < 0)
-		return (pid);
 	if (pid == 0)
 	{
-		command = bStruct->argv[bStruct->argc - 2];
-		execArr = getExecArr(command, bStruct->pathList);
-		if(!access(bStruct->argv[bStruct->argc - 1], 0))
-			fileFd = open(bStruct->argv[bStruct->argc - 1], O_TRUNC | O_RDWR); //открываем файл, из которого берём данные
+		command = bS->argv[bS->argc - 2];
+		execArr = getExecArr(command, bS->pathList);
+		if (!access(bS->argv[bS->argc - 1], 0))
+			fileFd = open(bS->argv[bS->argc - 1], O_TRUNC | O_RDWR);
 		else
-			fileFd = open(bStruct->argv[bStruct->argc - 1], O_CREAT | O_RDWR, 0644);
-		if (fileFd == -1)
-		{
-			printError(bStruct->argv[bStruct->argc - 1], 0);
-		}
-		dup2(fd[bStruct->commands_num - 1][0], STDIN_FILENO);
+			fileFd = open(bS->argv[bS->argc - 1], O_CREAT | O_RDWR, 0644);
+		checkFileFd(fileFd, bS->argv[bS->argc - 1], bS, fd);
+		dup2(fd[bS->commands_num - 1][0], STDIN_FILENO);
 		dup2(fileFd, STDOUT_FILENO);
 		close(fileFd);
-		bonusCloseAllFds(&fd, bStruct->commands_num);
+		bonusClean(fd, bS);
 		execve(execArr[0], execArr, NULL);
 		perror(NAME);
 		exit(1);
 	}
-	return (1);
+	return (pid);
 }
