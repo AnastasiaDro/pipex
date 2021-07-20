6ea503c99c0ddf54fb9d8d65f2cf0c7123f134ed
cerebus <cerebus@student.21-school.ru>
2021-07-20 15:22:29 +0300

    normed bonusParseCmd.c

--- bonusParseCmd.c
+++ bonusParseCmd.c
@@ -3,28 +3,28 @@
 #include "pipex.h"
 #include "libft/libft.h"
 
-int    bonusParseCmd(int ***fd, int current_index, t_bstruct *bStruct)
+int	bonusParseCmd(int ***fd, int current_index, t_bstruct *bStruct)
 {
-    int **tmp;
-    char *command;
-    char **execArr;
-    int coef;
+	int		**tmp;
+	char	*command;
+	char	**execArr;
+	int		coef;
 
-    if (bStruct->flag == HERE_DOC)
-        coef = 3;
-    else
-        coef = 2;
-    command = (bStruct->argv)[coef + current_index];
-    execArr = getExecArr(command, bStruct->pathList);
-    tmp = *fd;
-    if (execArr == NULL)
-    {
-        bonusCloseAllFds(fd, bStruct->commands_num);
-         exit(0);
-    }
-    dup2(tmp[current_index][0], STDIN_FILENO);
-    dup2(tmp[current_index + 1][1], STDOUT_FILENO);
-    bonusCloseAllFds(fd, bStruct->commands_num);
-    execve(execArr[0], execArr, NULL);
-    return (0);
+	if (bStruct->flag == HERE_DOC)
+		coef = 3;
+	else
+		coef = 2;
+	command = (bStruct->argv)[coef + current_index];
+	execArr = getExecArr(command, bStruct->pathList);
+	tmp = *fd;
+	if (execArr == NULL)
+	{
+		bonusCloseAllFds(fd, bStruct->commands_num);
+		 exit(0);
+	}
+	dup2(tmp[current_index][0], STDIN_FILENO);
+	dup2(tmp[current_index + 1][1], STDOUT_FILENO);
+	bonusCloseAllFds(fd, bStruct->commands_num);
+	execve(execArr[0], execArr, NULL);
+	return (0);
 }
